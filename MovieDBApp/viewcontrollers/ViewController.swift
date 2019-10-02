b0bc11b73f87baf7b86bd4b716c49905aed9218b
ShunLH <shunleihmu.shunlh@gmail.com>
2019-10-02 17:19:25 +0630

    first commit

---
+++ MovieDBApp/viewcontrollers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MovieDBApp
+//
+//  Created by AcePlus Admin on 10/2/19.
+//  Copyright © 2019 SLH. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
